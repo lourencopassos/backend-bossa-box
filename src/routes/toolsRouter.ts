cf15af2813335e083df94fcd7f0f7f58b5aad846
Lourenco <lo.passos93@gmail.com>
2021-03-28 20:16:28 -0300

    refactor: change endpoints router file

--- src/routes/toolsRouter.ts
+++ src/routes/toolsRouter.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import { ToolBusiness } from "../business/ToolBusiness";
 import { ToolController } from "../controller/ToolController";
+import { ToolDatabase } from "../data/ToolDatabase";
 
-export const toolRouter = express.Router();
+const toolRouter = express.Router();
 
-const toolController = new ToolController();
+const toolDatabase = new ToolDatabase()
+const toolBusiness = new ToolBusiness(toolDatabase)
+const toolController = new ToolController(toolBusiness);
 
 toolRouter.post("/", toolController.addTool);
 toolRouter.get("/", toolController.getAllTools);
 toolRouter.get("/filter", toolController.getToolsByTag);
 toolRouter.delete("/:id", toolController.deleteTool);
+
+
+export { toolRouter }
 
